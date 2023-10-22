64c178325c5ca8b981566e99027d33966ffb7c3b
felipe souza <felipe.macieldsouza@gmail.com>
2023-10-22 15:43:27 -0300

    inicio do projeto

---
+++ aws_ecommerce_order/src/main/java/com/workshopaws/aws_ecommerce_order/AwsEcommerceOrderApplication.java
@@ -0,0 +1,13 @@
+package com.workshopaws.aws_ecommerce_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEcommerceOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEcommerceOrderApplication.class, args);
+	}
+
+}
---
+++ aws_ecommerce_order/src/test/java/com/workshopaws/aws_ecommerce_order/AwsEcommerceOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopaws.aws_ecommerce_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEcommerceOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
